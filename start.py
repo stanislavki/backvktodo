063b7f7cd759ec53bb4aa0a07c0966a0d2673e43
stanislavki <naustas2007qwe@gmail.com>
2026-05-17 11:11:00 +0500

    fix10

--- start.py
+++ start.py
@@ -1,5 +1,9 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    # Запускаем uvicorn программно, указывая правильный путь к приложению
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=80)
+    # Переходим прямо в папку backend, чтобы Python видел config, database и т.д.
+    os.chdir("backend")
+    
+    # Теперь запускаем main:app напрямую, так как мы уже находимся внутри папки
+    uvicorn.run("main:app", host="0.0.0.0", port=80)
